b58b64c6f082bb2d04c7dbdd0069d4f2b0fc90dd
thang <thang@gmail.com>
2024-12-21 10:20:06 +0700

    Create Project

---
+++ Backend/BaiThiCuoiKy/BaiThiCuoiKy/src/main/java/com/ktck124/lop124ltdd04/nhom03/BaiThiCuoiKyApplication.java
@@ -0,0 +1,13 @@
+package com.ktck124.lop124ltdd04.nhom03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiThiCuoiKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiThiCuoiKyApplication.class, args);
+	}
+
+}
---
+++ Backend/BaiThiCuoiKy/BaiThiCuoiKy/src/test/java/com/ktck124/lop124ltdd04/nhom03/BaiThiCuoiKyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktck124.lop124ltdd04.nhom03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiThiCuoiKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
